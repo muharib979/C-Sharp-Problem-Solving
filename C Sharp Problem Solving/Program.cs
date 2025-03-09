f638414b1aebc0a74fec2f23a1e96a53852f1b5c
Muhammad Amran Bhuiyan <muharib979@gmail.com>
2025-03-10 00:29:05 +0600

    solve

---
+++ C Sharp Problem Solving/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+   
+
+    static void Main()
+    {
+        //1. Example: Get User Input
+
+        //Console.Write("Enter your Name: ");
+        //string name = Console.ReadLine();
+        //Console.Write("enterYour age: ");
+        //int age = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine($"Helle {name}, you are {age} your old ");
+
+
+        // 2.Check Even or Odd Number
+
+        //Console.Write("Enter a Number");
+        //int num = Convert.ToInt32(Console.ReadLine());
+
+        //if( num % 2 == 0)
+        //{
+        //    Console.WriteLine("Even Number");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Odd Number");
+        //}
+
+        // 3. Example: Simple Calculator
+
+        //Console.Write("Enter a Number");
+
+        //int num = Convert.ToInt32(Console.ReadLine());
+
+        //switch (num)
+        //{
+        //    case 1:
+        //        Console.WriteLine("One");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("Two");
+        //        break;
+
+        //        default:
+        //        Console.WriteLine("Other Number");
+        //        break;
+        //}
+    }
+}
